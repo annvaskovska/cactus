9f0841af2d6a44a7dfdd6ef059d8baca45a7351f
Ann Vaskovska <Ann Vaskovska>
2016-05-14 19:06:40 +0300

    'initial'

---
+++ ConspectusApp/src/client/app/app.module.js
@@ -0,0 +1,14 @@
+(function () {
+    'use strict';
+
+    angular.module('app', [
+        'app.core',
+        'app.widgets',
+        'app.dashboard',
+        'app.layout',
+        'app.landing',
+        'app.apis',
+        'ngResource'
+    ]);
+
+})();
