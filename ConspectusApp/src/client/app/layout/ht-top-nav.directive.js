5a70ec7f05c62f93b4c1c173613d24dcb5cf5319
Ann Vaskovska <annavaskov@gmail.com>
2016-05-28 21:30:59 +0300

    add creation and editing date

--- ConspectusApp/src/client/app/layout/ht-top-nav.directive.js
+++ ConspectusApp/src/client/app/layout/ht-top-nav.directive.js
@@ -39,7 +39,21 @@
                 console.log(args.lecture);
                 vm.lecture = args.lecture;
                 vm.subject = args.subject;
+                vm.lecture = setCreateAndUpdateFields(vm.lecture);
             });
+
+            function setCreateAndUpdateFields(lecture) {
+                var createDate = new Date(lecture.createDate);
+                var updateDate = new Date(lecture.createDate);
+
+                var createString = 'Створено: ' +createDate.getDate() + ' ' + setMonth(createDate.getDate()) + ' ' + createDate.getFullYear() + ' o ' + createDate.getHours() + ':' + createDate.getMinutes();
+                var updateString = 'Редаговано: ' + updateDate.getDate() + ' ' + setMonth(updateDate.getDate()) + ' ' + updateDate.getFullYear() + ' o ' + updateDate.getHours() + ':' + updateDate.getMinutes();
+
+                lecture.created = createString;
+                lecture.updated = updateString;
+
+                return lecture;
+            }
 
             function isCurrent(route) {
                 if (!$state.current || !$state.current.title) {
@@ -50,8 +64,27 @@
             }
 
             function logOut() {
-                user.deauthorize();
                 $state.go('landing.home');
+            }
+
+            function setMonth(n) {
+                switch(n) {
+                    case 0: return 'січня';
+                    case 1: return 'лютого';
+                    case 2: return 'березня';
+                    case 3: return 'квітня';
+                    case 4: return 'травня';
+                    case 5: return 'червня';
+                    case 6: return 'липня';
+                    case 7: return 'серпня';
+                    case 8: return 'вересня';
+                    case 9: return 'жовтня';
+                    case 10: return 'листопада';
+                    case 11: return 'грудня';
+
+                    default:
+                    return 'травня';
+                }
             }
 
         }
